38a5402bbeddeddee750b4772827f6091b1e3c2f
NITTAGANESH <nittaganeshkumar@gmail.com>
2024-08-09 14:25:41 +0530

    updated app config

--- HRMS/src/main/java/com/SPYDTECH/HRMS/configuration/AppConfig.java
+++ HRMS/src/main/java/com/SPYDTECH/HRMS/configuration/AppConfig.java
@@ -99,9 +99,9 @@ public class AppConfig {
             cfg.setAllowedOrigins(Arrays.asList(
                     "http://3.111.147.73:3000",
                     "http://3.111.147.73:3001",
-                    "http://localhost:5173",
+                    "http://localhost:5174",
                     "http://3.111.147.73:8080",
-                    "http://3.111.147.73:3306"
+                    "http://3.111.147.73:5173"
             ));
             cfg.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
             cfg.setAllowCredentials(true);
