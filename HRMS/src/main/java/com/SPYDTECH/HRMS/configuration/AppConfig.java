da79121bbbd5ad9f03b2578d14d47f44190b5009
NITTAGANESH <nittaganeshkumar@gmail.com>
2024-08-14 02:04:07 +0530

    holidays done

--- HRMS/src/main/java/com/SPYDTECH/HRMS/configuration/AppConfig.java
+++ HRMS/src/main/java/com/SPYDTECH/HRMS/configuration/AppConfig.java
@@ -18,54 +18,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 
 import java.util.Arrays;
 
-//@Configuration
-//@EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
-//public class AppConfig {
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .authorizeHttpRequests(authorize-> authorize
-//                        .requestMatchers("/api/**").authenticated()
-//                        .anyRequest().permitAll())
-//                .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
-//                .csrf().disable()
-//                .cors().configurationSource(corsConfigurationSource())
-//                .and()
-//                .httpBasic()
-//                .and()
-//                .formLogin()
-//
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource(){
-//        return request -> {
-//            CorsConfiguration cfg = new CorsConfiguration();
-//            cfg.setAllowedOrigins(Arrays.asList(
-//                    "http://localhost:3000",
-//                    "http://localhost:3001",
-//                    "http://localhost:4200"
-//            ));
-//            cfg.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-//            cfg.setAllowCredentials(true);
-//            cfg.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-//            cfg.setExposedHeaders(Arrays.asList("Authorization"));
-//            cfg.setMaxAge(3600l);
-//            return cfg;
-//        };
-//
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder(){
-//        return new BCryptPasswordEncoder();
-//    }
-//}
-
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -100,6 +52,7 @@ public class AppConfig {
                     "http://3.111.147.73:3000",
                     "http://3.111.147.73:3001",
                     "http://localhost:5174",
+                    "http://localhost:5173",
                     "http://3.111.147.73:8080",
                     "http://3.111.147.73:5173",
                     "http://13.234.49.187:5173"
---
+++ HRMS/src/main/java/com/SPYDTECH/HRMS/service/HolidayService.java
@@ -0,0 +1,13 @@
+package com.SPYDTECH.HRMS.service;
+
+import com.SPYDTECH.HRMS.entites.Employee;
+import com.SPYDTECH.HRMS.entites.HolidaysList;
+
+import java.util.List;
+
+public interface HolidayService {
+    boolean deleteHoliday(String name);
+    public List<HolidaysList> getAllHolidays();
+    HolidaysList save(HolidaysList holidaysList);
+    HolidaysList updateHoliday(String name, HolidaysList holidaysListDetails);
+}
